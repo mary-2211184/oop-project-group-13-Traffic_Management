1322e30e6abfe05596ac0ffbdab5ba4ba18068fe
mary-2211184 <maryrhym@gmail.com>
2025-11-11 12:11:33 +0600

    first commit

---
+++ src/main/resources/com/group13/oopprojectgroup13traffic_management/Mary/IncidentOfficerController.java
@@ -0,0 +1,5 @@
+public class IncidentOfficerController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/resources/com/group13/oopprojectgroup13traffic_management/Mary/TrafficEnforcementOfficerController.java
@@ -0,0 +1,5 @@
+public class TrafficEnforcementOfficerController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
