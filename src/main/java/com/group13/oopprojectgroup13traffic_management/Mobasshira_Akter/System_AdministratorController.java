86e4aa36c3adceee73eaa78f175f78538d190987
mobasshira13 <mobasshira8@gmail.com>
2025-11-11 12:28:16 +0600

    name change

---
+++ src/main/java/com/group13/oopprojectgroup13traffic_management/Mobasshira_Akter/System_AdministratorController.java
@@ -0,0 +1,7 @@
+package com.group13.oopprojectgroup13traffic_management.Mobasshira_Akter;
+
+public class System_AdministratorController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/com/group13/oopprojectgroup13traffic_management/Mobasshira_Akter/Traffic_Control_AdminController.java
@@ -0,0 +1,7 @@
+package com.group13.oopprojectgroup13traffic_management.Mobasshira_Akter;
+
+public class Traffic_Control_AdminController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
