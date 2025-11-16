d0868f42e577e3ba3cc3f70174e8c0c8fca74fa5
mary-2211184 <maryrhym@gmail.com>
2025-11-16 23:15:16 +0600

    first commit

---
+++ src/main/java/com/group13/oopprojectgroup13traffic_management/MaryChowdhury/EditReportController.java
@@ -0,0 +1,29 @@
+package com.group13.oopprojectgroup13traffic_management.MaryChowdhury;
+
+public class EditReportController
+{
+    @javafx.fxml.FXML
+    private Label loadLabel;
+    @javafx.fxml.FXML
+    private TextField locationField;
+    @javafx.fxml.FXML
+    private ComboBox comboReportSelector;
+    @javafx.fxml.FXML
+    private ComboBox comboType;
+    @javafx.fxml.FXML
+    private TextField timeField;
+    @javafx.fxml.FXML
+    private TextArea descTextArea;
+    @javafx.fxml.FXML
+    private DatePicker datePicker;
+    @javafx.fxml.FXML
+    private Label resultLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void onSaveChanges(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/group13/oopprojectgroup13traffic_management/MaryChowdhury/UploadEvidenceController.java
@@ -0,0 +1,23 @@
+package com.group13.oopprojectgroup13traffic_management.MaryChowdhury;
+
+public class UploadEvidenceController
+{
+    @javafx.fxml.FXML
+    private TextField reportIdField;
+    @javafx.fxml.FXML
+    private Label uploadStatusLabel;
+    @javafx.fxml.FXML
+    private ListView fileListView;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void onUploadFiles(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onChooseFiles(ActionEvent actionEvent) {
+    }
+}
