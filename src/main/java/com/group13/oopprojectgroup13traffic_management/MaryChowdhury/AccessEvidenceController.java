c7678591af9f409c5dd48451f1ab7e4990234c2f
mary-2211184 <maryrhym@gmail.com>
2025-11-24 12:33:21 +0600

    first commit

--- src/main/java/com/group13/oopprojectgroup13traffic_management/MaryChowdhury/AccessEvidenceController.java
+++ src/main/java/com/group13/oopprojectgroup13traffic_management/MaryChowdhury/AccessEvidenceController.java
@@ -3,5 +3,25 @@ package com.group13.oopprojectgroup13traffic_management.MaryChowdhury;
 public class AccessEvidenceController
 {
     @javafx.fxml.FXML
+    private ComboBox reportSelectorComboBox;
+    @javafx.fxml.FXML
+    private ListView evidenceList;
+    @javafx.fxml.FXML
+    private Label evidenceStatusLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void oaDownload(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void oaViewSelected(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void oaLoadEvidence(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/group13/oopprojectgroup13traffic_management/MaryChowdhury/AssignReportController.java
+++ src/main/java/com/group13/oopprojectgroup13traffic_management/MaryChowdhury/AssignReportController.java
@@ -3,5 +3,19 @@ package com.group13.oopprojectgroup13traffic_management.MaryChowdhury;
 public class AssignReportController
 {
     @javafx.fxml.FXML
+    private ComboBox officerSelectorComboBox;
+    @javafx.fxml.FXML
+    private ComboBox reportSelectorComboBox;
+    @javafx.fxml.FXML
+    private Label reportInfoLabel;
+    @javafx.fxml.FXML
+    private Label assignStatusLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void oaAssign(ActionEvent actionEvent) {
+    }
+}
