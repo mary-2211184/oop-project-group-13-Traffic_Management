299fc3af15286ed61ae2ae089e18bbfed12bac17
mary-2211184 <maryrhym@gmail.com>
2025-11-24 12:24:33 +0600

    first commit

--- src/main/java/com/group13/oopprojectgroup13traffic_management/MaryChowdhury/OfficerLoginController.java
+++ src/main/java/com/group13/oopprojectgroup13traffic_management/MaryChowdhury/OfficerLoginController.java
@@ -3,5 +3,17 @@ package com.group13.oopprojectgroup13traffic_management.MaryChowdhury;
 public class OfficerLoginController
 {
     @javafx.fxml.FXML
+    private TextField usernameField;
+    @javafx.fxml.FXML
+    private TextField passwordField;
+    @javafx.fxml.FXML
+    private Label loginStatusLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void oaLogin(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/group13/oopprojectgroup13traffic_management/MaryChowdhury/UpdateStatusController.java
+++ src/main/java/com/group13/oopprojectgroup13traffic_management/MaryChowdhury/UpdateStatusController.java
@@ -3,5 +3,19 @@ package com.group13.oopprojectgroup13traffic_management.MaryChowdhury;
 public class UpdateStatusController
 {
     @javafx.fxml.FXML
+    private ComboBox selectReporterComboBox;
+    @javafx.fxml.FXML
+    private Label currentStatusLabel;
+    @javafx.fxml.FXML
+    private ComboBox statusComboBox;
+    @javafx.fxml.FXML
+    private Label resultLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void oaChangeStatus(ActionEvent actionEvent) {
+    }
+}
