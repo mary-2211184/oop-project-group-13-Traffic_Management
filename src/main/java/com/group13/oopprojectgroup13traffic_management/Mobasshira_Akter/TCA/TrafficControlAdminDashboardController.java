04c91a802945a7105bd9e08396a910097e54feb7
mobasshira13 <mobasshira8@gmail.com>
2025-11-27 03:51:28 +0600

    1st commit on right cloned project.

--- src/main/java/com/group13/oopprojectgroup13traffic_management/Mobasshira_Akter/TCA/TrafficControlAdminDashboardController.java
+++ src/main/java/com/group13/oopprojectgroup13traffic_management/Mobasshira_Akter/TCA/TrafficControlAdminDashboardController.java
@@ -3,5 +3,45 @@ package com.group13.oopprojectgroup13traffic_management.Mobasshira_Akter.TCA;
 public class TrafficControlAdminDashboardController
 {
     @javafx.fxml.FXML
+    private AnchorPane aP;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void fcOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void tfOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void anOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void bOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void rOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void imOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void soOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void tmOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void hdOA(ActionEvent actionEvent) {
+    }
+}
