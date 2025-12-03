625f7096bd5865ad83c28ff1dd5ac74128253321
LimoMoni <limukhadiza@gmail.com>
2025-12-03 18:40:44 +0530

    Commit 3

--- src/main/java/com/group13/oopprojectgroup13traffic_management/Limu/policymaker/TrafficGuidelinesDevelopmentController.java
+++ src/main/java/com/group13/oopprojectgroup13traffic_management/Limu/policymaker/TrafficGuidelinesDevelopmentController.java
@@ -1,11 +1,14 @@
 package com.group13.oopprojectgroup13traffic_management.Limu.policymaker;
 
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class TrafficGuidelinesDevelopmentController
 {
     @javafx.fxml.FXML
     private TextArea demoTextArea;
+    @javafx.fxml.FXML
+    private TextField textField;
 
     @javafx.fxml.FXML
     public void initialize() {
