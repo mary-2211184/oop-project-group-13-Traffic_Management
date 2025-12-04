c34a0286fdfc7360050c23fdd53f9566e05e44c5
Maruf Hossain Rafi <2222222@iub.edu.bd>
2025-12-04 17:59:35 +0600

    fix module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,4 +10,16 @@ module com.group13.oopprojectgroup13traffic_management {
     exports com.group13.oopprojectgroup13traffic_management.Limu.ParkingsystemManager;
     opens com.group13.oopprojectgroup13traffic_management.Limu.policymaker to javafx.fxml;
     exports com.group13.oopprojectgroup13traffic_management.Limu.policymaker;
+
+    opens com.group13.oopprojectgroup13traffic_management.Rafi to javafx.fxml;
+    exports com.group13.oopprojectgroup13traffic_management.Rafi;
+
+    opens com.group13.oopprojectgroup13traffic_management.MaryChowdhury to javafx.fxml;
+    exports com.group13.oopprojectgroup13traffic_management.MaryChowdhury;
+
+    opens com.group13.oopprojectgroup13traffic_management.Mobasshira_Akter.SA to javafx.fxml;
+    exports com.group13.oopprojectgroup13traffic_management.Mobasshira_Akter.SA;
+
+    opens com.group13.oopprojectgroup13traffic_management.Mobasshira_Akter.TCA to javafx.fxml;
+    exports com.group13.oopprojectgroup13traffic_management.Mobasshira_Akter.TCA;
 }
