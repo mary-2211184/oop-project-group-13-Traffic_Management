b41b9a447bd498dcb40f2e044305f61f0d950aeb
Maruf Hossain Rafi <2222222@iub.edu.bd>
2025-12-06 02:19:55 +0600

    bug fixing

--- src/main/java/com/group13/oopprojectgroup13traffic_management/HelloApplication.java
+++ src/main/java/com/group13/oopprojectgroup13traffic_management/HelloApplication.java
@@ -1,27 +1,4 @@
-<<<<<<< HEAD
-package com.group13.oopprojectgroup13traffic_management;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/group13/oopprojectgroup13traffic_management/Limu/ParkingsystemManager/parkingSystemDashboard.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
-=======
 package com.group13.oopprojectgroup13traffic_management;
 
 import javafx.application.Application;
@@ -36,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Rafi/log-in.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Traffic Management App");
+        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
@@ -44,5 +21,5 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
->>>>>>> a193039ac47c5b86272589ded815e69347d8335e
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 module com.group13.oopprojectgroup13traffic_management {
     requires javafx.controls;
     requires javafx.fxml;
@@ -11,19 +11,7 @@ module com.group13.oopprojectgroup13traffic_management {
     exports com.group13.oopprojectgroup13traffic_management.Limu.ParkingsystemManager;
     opens com.group13.oopprojectgroup13traffic_management.Limu.policymaker to javafx.fxml;
     exports com.group13.oopprojectgroup13traffic_management.Limu.policymaker;
-=======
-module com.group13.oopprojectgroup13traffic_management {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires jdk.hotspot.agent;
 
-
-    opens com.group13.oopprojectgroup13traffic_management to javafx.fxml;
-    exports com.group13.oopprojectgroup13traffic_management;
-    opens com.group13.oopprojectgroup13traffic_management.Limu.ParkingsystemManager to javafx.fxml;
-    exports com.group13.oopprojectgroup13traffic_management.Limu.ParkingsystemManager;
-    opens com.group13.oopprojectgroup13traffic_management.Limu.policymaker to javafx.fxml;
-    exports com.group13.oopprojectgroup13traffic_management.Limu.policymaker;
 
     opens com.group13.oopprojectgroup13traffic_management.Rafi to javafx.fxml;
     exports com.group13.oopprojectgroup13traffic_management.Rafi;
@@ -36,5 +24,4 @@ module com.group13.oopprojectgroup13traffic_management {
 
     opens com.group13.oopprojectgroup13traffic_management.Mobasshira_Akter.TCA to javafx.fxml;
     exports com.group13.oopprojectgroup13traffic_management.Mobasshira_Akter.TCA;
->>>>>>> a193039ac47c5b86272589ded815e69347d8335e
 }
